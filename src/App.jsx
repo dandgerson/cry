22b55da957d7d9531909890eb5dd4d86fc35cf3c
Dmitry G. Anderson <dandgerson@gmail.com>
2020-10-10 15:56:47 +0300

    add styles structure & add classnames

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import cl from 'classnames'
 import s from './App.module.scss'
 
 function App() {
   return (
-    <div className="App" />
+    <div className={cl(s.App)}>
+      <h1>Hello Cry App</h1>
+    </div>
   )
 }
 
