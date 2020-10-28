80d67e4846a9eb62e93b7030dcb24ab83084fed2
Dmitry G. Anderson <dandgerson@gmail.com>
2020-10-29 01:48:57 +0300

    use Draft.js

--- src/App/components/Feed/CryBox/index.jsx
+++ src/App/components/Feed/CryBox/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import cl from 'classnames'
+import { Editor, EditorState, ContentState } from 'draft-js'
 
 import Button from 'App/components/Button'
 import Avatar from 'App/components/Avatar'
@@ -15,13 +16,17 @@ import everyoneIcon from 'images/everyone-icon.svg'
 import peopleIcon from 'images/people-icon.svg'
 import mentionIcon from 'images/mention-icon.svg'
 
+import 'draft-js/dist/Draft.css'
+
 import s from './CryBox.module.scss'
 import t from './dark-theme.module.scss'
 
 const CryBox = () => {
   const [isFocused, setIsFocused] = useState(false)
   const [activeReply, setActiveReply] = useState('everyone')
-  const [inputValue, setInputValue] = useState('')
+  const [editorState, setEditorState] = useState(() => EditorState.createWithContent(ContentState.createFromText('What\'s happened?')))
+
+  console.log({ editorState })
 
   const controlsMap = {
     addImage: pictureIcon,
@@ -61,15 +66,14 @@ const CryBox = () => {
 
       <form>
         <div className={cl(
-          s.input,
-          t.input,
+          s.editor,
+          t.editor,
         )}
         >
-          <input
-            placeholder="What's happened?"
-            type='text'
+          <Editor
+            editorState={editorState}
+            onChange={setEditorState}
             onFocus={() => setIsFocused(true)}
-            onChange={e => setInputValue(e.target.value)}
           />
         </div>
 
@@ -108,20 +112,47 @@ const CryBox = () => {
           >
             {Object.keys(controlsMap).map(control => (
               <React.Fragment key={control}>
-                <ActionIcon
-                  src={controlsMap[control]}
-                  width={24}
-                  height={24}
-                  rootClasses={[
-                    t.controls_icon,
-                  ]}
-                  accentColor={t.darkAccentColor}
-                />
+                {control === 'addImage'
+                  ? (
+                    <div>
+                      <label htmlFor='add-image'>
+                        <ActionIcon
+                          src={controlsMap[control]}
+                          width={24}
+                          height={24}
+                          rootClasses={[
+                            t.controls_icon,
+                          ]}
+                          accentColor={t.darkAccentColor}
+                        />
+                      </label>
+                      <input
+                        id='add-image'
+                        type='file'
+                        accept='image/png, image/jpeg'
+                        style={{
+                          display: 'none',
+                        }}
+                      />
+
+                    </div>
+                  )
+                  : (
+                    <ActionIcon
+                      src={controlsMap[control]}
+                      width={24}
+                      height={24}
+                      rootClasses={[
+                        t.controls_icon,
+                      ]}
+                      accentColor={t.darkAccentColor}
+                    />
+                  )}
               </React.Fragment>
             ))}
           </div>
           <Button
-            isActive={Boolean(inputValue.length)}
+            isActive={editorState.getCurrentContent().hasText()}
           />
         </div>
       </form>
